13f212c7d7a8118f55a05b035f5de81326fe4027
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-08-11 22:09:25 -0300

    commit do final de semana, voltando estudo node

--- teste1/teste.js
+++ teste1/teste.js
@@ -1,1 +1,31 @@
-console.log("criando rep")
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log("CHEGOU NA API")
+    console.log(req.method)
+    console.log(req.url)
+
+    if(req.method === 'GET' && req.url.startsWith('/user')){
+        res.writeHead(200)
+        res.end('todos os usúarios')
+        console.log("Req get recebida")
+        return
+    }
+
+    if(req.method === 'POST' && req.url.startsWith('/user')){
+        res.writeHead(200)
+        res.end('todos os usúarios')
+        return
+    }
+    
+
+    res.writeHead(404)
+    res.end("recurso não encontrado")
+
+})
+
+server.listen(3000, () => {
+    console.log("servidor na porta 3000")
+})
+
+

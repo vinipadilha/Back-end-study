aeebdebcd4a56014dd5860f3edbf39037cc4b628
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-09-08 22:37:18 -0300

    criando API do zero

--- APIsExpress/index.js
+++ APIsExpress/index.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.use(express.json());
+const app = express()
 
-app.get('/alunos', (req, res) => {
-    console.log("entrou no alunos")
-    res.status(200).send('deu certo')
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
 
+app.listen(21262, () => {
+    console.log(' Express strated at http://localhost:21262')
 })
 
-app.listen(3000, () => {
-    console.log('iniciei meu servidor')
+
+let db =  [
+    { '1': { Nome: 'Cliente 1 ', Idade: '20' } },
+    { '2': { Nome: 'Cliente 2 ', Idade: '20' } },
+    { '3': { Nome: 'Cliente 3 ', Idade: '20' } }
+]
+
+
+app.get('/', (req, res) => {
+    
 })

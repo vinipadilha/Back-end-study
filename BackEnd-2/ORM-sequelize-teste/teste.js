ecf2ec0dec5dd29c90ebd59d14c47bee28859568
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-10-09 19:16:01 -0300

    Configuração de conexão com banco de dados

--- BackEnd-2/ORM-sequelize-teste/teste.js
+++ BackEnd-2/ORM-sequelize-teste/teste.js
@@ -1,12 +1,11 @@
 
-
+// Configuração de conexão com o banco de dados
 const { Sequelize } =  require('sequelize')
 
-const sequelize = new Sequelize ('nome do banco', 'nome user', 'senha',{
-    host: 'endpoint', 
+const sequelize = new Sequelize ('aulabd-curso', 'Vinicius Padilha', 'admin123',{
+    host: 'aulabd-curso.cj46cgswgbca.us-east-1.rds.amazonaws.com', 
     dialect: 'postgres'
 })
 
 
 module.exports = sequelize
-
